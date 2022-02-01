b6cc32936ca87eb8c34bded824078dbc3f9b8c0e
adesoft <adesholly11@gmail.com>
2022-02-01 23:25:52 +0100

    setting up the project

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+
+  return (
+    <div>
+      <nav className="bg-white shadow-lg">
+       
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
