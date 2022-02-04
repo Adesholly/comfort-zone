89399af6df81b47cd6959dc2fd781bcd07966594
adesoft <adesholly11@gmail.com>
2022-02-04 22:04:44 +0100

    Setting up the backend

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,49 +5,58 @@ function Navbar() {
     <div>
       <nav className="bg-white shadow-lg">
         <div className="max-w-6xl mx-auto px-4">
-          
-            <div className="flex justify-between">
+          <div className="flex justify-between">
+            <div>
+              <a className="flex items-center py-4 px-2">
+                <img src="comZoneLogo.png" className="h-10 w-10 mr-2" />
+                <span className="font-bold hover:text-gray-800 text-lg">
+                  Comfort Zone
+                </span>
+              </a>
+            </div>
 
-              <div>
-                <a className="flex items-center py-4 px-2">
-                  <img src="comZoneLogo.png" className="h-10 w-10 mr-2" />
-                  <span className="font-bold hover:text-gray-800 text-lg">
-                    Comfort Zone
-                  </span>
-                </a>
-              </div>
-
-          
-              <div className="hidden sm:flex items-center space-x-1">
-                <a
-                  href=""
-                  className="py-2 px-2 font-medium text-gray-500 hover:text-gray-800"
-                >
-                  Log In
-                </a>
-                <a
-                  href=""
-                  className="py-2 px-2 font-medium text-gray-500  hover:text-gray-800 hover:f ont-semibold"
-                >
-                  Register
-                </a>
-              </div>
-            
-
+            <div className="hidden sm:flex items-center space-x-1">
+              <a
+                href=""
+                className="py-2 px-2 font-medium text-gray-500 hover:text-gray-800"
+              >
+                Log In
+              </a>
+              <a
+                href=""
+                className="py-2 px-2 font-medium text-gray-500  hover:text-gray-800 hover:f ont-semibold"
+              >
+                Register
+              </a>
             </div>
 
             {/*  Mobile menu button */}
-            <div className="md:hidden flex items-center">
+            <div className="sm:hidden flex items-center">
               <button className="outline-none mobile-menu-button">
-
-                <svg className="">
-                  <path d= "M4 6h16M4 12h16M4 18h16"></path>
+                <svg
+                className="w-6 h-6 text-gray-800"
+                x-show="!showMenu"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                  <path d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>
-
               </button>
             </div>
-           
-         
+
+          </div>      
+        </div>
+        <div className="hidden mobile-menu">
+          <ul>
+            <li>
+              <a></a>
+            </li>
+          </ul>
+
         </div>
       </nav>
     </div>
